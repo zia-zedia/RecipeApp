d3f6297bf4b44d464768ad09543c4e22e0d48baa
Group102 <ziagallaf578@gmail.com>
2022-12-23 22:12:37 +0300

    Tab controller, category page, placeholders for other pages.

---
+++ RecipeApp/categoryCell.swift
@@ -0,0 +1,29 @@
+//
+//  categoryCell.swift
+//  RecipeApp
+//
+//  Created by moh on 23/12/2022.
+//
+
+import UIKit
+
+class categoryCell: UITableViewCell {
+
+    
+    @IBOutlet weak var categoryBackground: UIView!
+    @IBOutlet weak var categoryLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10))
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
